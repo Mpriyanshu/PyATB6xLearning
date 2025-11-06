037ac6b23b5f742085e4280d4c62b16cbdb79afb
new <tiwaripriyanshu640@gmail.com>
2025-11-06 20:11:59 +0530

    6th nov code Added

---
+++ src/ex_14_Tuple/Lab100_Tuple.py
@@ -0,0 +1,19 @@
+shopping_list_wife = ["bread", "butter", "paneer"]
+shopping_list_wife[2] = "milk"
+print(shopping_list_wife)
+
+# Real of Tuples
+my_tuple = ("tta.com", "sdet.live")
+my_api_list = list(my_tuple)
+print(my_api_list)
+
+# Real case, where we Tuples
+API_URLSs = ("https://sdet.live/python0x", "https://awesomeqa. com")
+print(API_URLSs[0])
+print(API_URLSs[1])
+
+t = tuple()
+print(t)
+
+l = list()
+print(l)
