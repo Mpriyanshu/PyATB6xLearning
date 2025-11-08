e5698d2ca9baf3d262c2deabf650a719ae0440b1
new <tiwaripriyanshu640@gmail.com>
2025-11-08 19:40:57 +0530

    8th nov code Added

---
+++ src/ex_15_Set/Lab104_SET_P3.py
@@ -0,0 +1,10 @@
+set1 = set(["TheTestingAcademy", "For", "TheTestingAcademy."])
+print(set1)
+print(len(set1))
+
+for i in set1:
+    print(i)
+
+set1.add("Priyanshu")
+set1.add("Priyanshu")
+print(set1)
