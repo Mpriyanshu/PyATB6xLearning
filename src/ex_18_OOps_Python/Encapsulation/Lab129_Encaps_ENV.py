e4a4c90c0a610c9bab34452d62822af69ec12ab5
new <tiwaripriyanshu640@gmail.com>
2025-11-25 21:25:44 +0530

    18th nov code Added

--- src/ex_18_OOps_Python/Encapsulation/Lab129_Encaps_ENV.py
+++ src/ex_18_OOps_Python/Encapsulation/Lab129_Encaps_ENV.py
@@ -1,11 +1,25 @@
+from dotenv import load_dotenv # from python community -> use from
+import os # from python guys -> use import
 class VWOLoginPage:
 
     def __init__(self, email_arg,password_arg):
+
         self.email = email_arg
         self.password = password_arg
 
     def login_confirm(self):
-        if self.email == "pt640@gmail.com" and self.password == "Pt@12":
-            print("Allowed to Login")
+        load_dotenv()
+
+        print("ENV USERNAME:", os.getenv("USERNAME"))
+        print("ENV PASSWORD:", os.getenv("PASSWORD"))
+
+        if self.email == os.getenv("USERNAME") and self.password == os.getenv("PASSWORD"):
+            print("Allowed, Login Sucessfull")
         else:
             print("Login Failed")
+
+email = input("Enter the vwo login email: ")
+password = input("Enter the vwo login password: ")
+
+vwo_object_ref = VWOLoginPage(email,password)
+vwo_object_ref.login_confirm()
