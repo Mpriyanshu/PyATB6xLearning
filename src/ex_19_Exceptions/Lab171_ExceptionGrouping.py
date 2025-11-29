ce7f4eb4d9deec0def95b4b6af4bc19271238e09
new <tiwaripriyanshu640@gmail.com>
2025-11-29 21:32:58 +0530

    27th nov code Added

---
+++ src/ex_19_Exceptions/Lab171_ExceptionGrouping.py
@@ -0,0 +1,10 @@
+# Python 3.11
+eg = ExceptionGroup("Multiple Ex",[
+    ValueError("Invalid Value"),
+    TypeError("Type Error"),
+    ZeroDivisionError("Can't div Xero")
+])
+
+def check_div(a):
+    if a == 0:
+        raise eg
