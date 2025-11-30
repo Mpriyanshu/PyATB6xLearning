39ab91b646ef70a7e86e443d3a3fa0348b88ed1a
new <tiwaripriyanshu640@gmail.com>
2025-11-30 21:28:28 +0530

    29th nov code Added

---
+++ src/ex_20_Modules_OS/Lab172_OS.py
@@ -0,0 +1,15 @@
+import os
+
+print(os.name)
+print(os.getcwd())
+#print(os.mkdir("AI"))
+
+print(os.listdir())
+
+#print(os.remove("AI.txt"))
+#print(os.rename("AI.txt","testdata.txt"))
+
+#import requests
+#data = requests.get("https://www.python.org/")
+
+print(os.environ.get("PATH"))
